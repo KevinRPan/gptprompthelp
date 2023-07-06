0faf035bac396395d51e49cf79a38c7787e000e8
kevin pan <kevin.pan@strivr.com>
2023-07-06 10:06:12 -0400

    turn off answer helper for initial

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ if user_input:
     st.info("Original prompt: " + user_input)
     # col1a,col1b = st.columns([1,1])
     st.markdown("**Original input, Standard Answer**")
-    a1=answer_prompt(user_input)
+    a1=answer_prompt(user_input,system_instructions="")
     st.write(a1)
     
     
